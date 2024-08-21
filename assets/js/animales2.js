b3c03b759188ebc654a757bf039a9c42e44ab314
FranciscoPuas <francisco.puasv@gmail.com>
2024-08-21 11:23:20 -0400

    se modifica index.js y animales.js para corregir errores

---
+++ assets/js/animales2.js
@@ -0,0 +1,85 @@
+class Animal {
+  #nombre;
+  #edad;
+  #img;
+  #comentarios;
+  #sonido;
+
+  constructor(nombre, edad, img, comentarios, sonido) {
+    this.#nombre = nombre;
+    this.#edad = edad;
+    this.#img = img;
+    this.#comentarios = comentarios;
+    this.#sonido = sonido;
+  }
+
+  get Nombre() {
+    return this.#nombre;
+  }
+
+  get Edad() {
+    return this.#edad;
+  }
+
+  get Img() {
+    return this.#img;
+  }
+
+  set Comentarios(comentarios) {
+    this.#comentarios = comentarios;
+  }
+
+  get Sonido() {
+    return this.#sonido;
+  }
+}
+
+class Leon extends Animal {
+  constructor(nombre, edad, img, comentarios, sonido) {
+    super(nombre, edad, img, comentarios, sonido);
+  }
+
+  Rugir() {
+    console.log("El león ruge");
+  }
+}
+
+class Lobo extends Animal {
+  constructor(nombre, edad, img, comentarios, sonido) {
+    super(nombre, edad, img, comentarios, sonido);
+  }
+
+  Aullar() {
+    console.log("El lobo aúlla");
+  }
+}
+
+class Oso extends Animal {
+  constructor(nombre, edad, img, comentarios, sonido) {
+    super(nombre, edad, img, comentarios, sonido);
+  }
+
+  Gruñir() {
+    console.log("El oso gruñe");
+  }
+}
+
+class Serpiente extends Animal {
+  constructor(nombre, edad, img, comentarios, sonido) {
+    super(nombre, edad, img, comentarios, sonido);
+  }
+
+  Sisear() {
+    console.log("La serpiente sisea");
+  }
+}
+
+class Aguila extends Animal {
+  constructor(nombre, edad, img, comentarios, sonido) {
+    super(nombre, edad, img, comentarios, sonido);
+  }
+
+  Chillar() {
+    console.log("El águila chilla");
+  }
+}
